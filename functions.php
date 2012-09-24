7b797283431e03e5e72203f06f79d362becf7ebb
Ryan Regalado <ryan@design48.net>
2012-09-23 20:01:56 -0700

    getenv variable

--- functions.php
+++ functions.php
@@ -14,8 +14,8 @@ function isXHR() {
  */
 function connect() {
 	global $pdo;
-    // $pdo = new PDO("mysql:host=sakiladesign48.db.6897065.hostedresource.com;dbname=sakiladesign48", "sakiladesign48", "Onerocks13");
-    $pdo = new PDO(EXTERNAL_DATABASE_URL);
+    $dbUrl = getenv('EXTERNAL_DATABASE_URL');
+    $pdo = new PDO($dbUrl);
 }
 
 /**
